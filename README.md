639b78605603a51762c7257b8ac45fac9f922399
Mie <n.vanessamie@gmail.com>
2022-10-01 02:01:15 -0300

    validações e mensagens de erro customizadas

--- README.md
+++ README.md
@@ -34,6 +34,23 @@ Usar regras de regex dentro do atributo pattern para validar o campo de senha.
 Mandar uma mensagem customizada de erro no balão do navegador.
 
 
+Criar funções para validações customizadas do formulário
+
+Definir mensagens customizadas para validações fora do HTML
+
+Utilizar data attributes para trabalhar com JavaScript
+
+Deixar a função de validação mais genérica para funcionar com qualquer input
+A instalar e usar o plugin Liveserver
+
+Como baixar e rodar o browser-sync
+
+Como mostrar a mensagem de erro diretamente no HTML
+
+Como customizar as mensagens de erro de validação
+
+
+
 abrir a pasta admin no vscode e 
 rodar o json server: 
 ```js
--- assets/js/validate/validaCliente.js
+++ assets/js/validate/validaCliente.js
@@ -1,11 +1,57 @@
-// const dataNascimento = document.querySelector('#nascimento')
 
-// dataNascimento.addEventListener('blur', (evento) => {
-//     validaDataNascimento(evento.target)
-// })
-
-const valida = (input) => {
+export const valida = (input) => {
     const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {   
+        valueMissing: 'O campo Nome não pode estar vazio'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio',
+        typeMismatch: 'O campo digitado não é válido'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio',
+        patternMismatch: 'A senha deve conter de 6 a 12 caracteres, pelo menos 1 letra maiúscula e 1 número e não deve conter símbolos'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio',
+        customError: 'Você deve ser maior de idade para se cadastrar!'
+    }
+}
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
+
+const mostraMensagemDeErro = (tipoDeInput, input) => {
+    let mensagem = ''
+
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro])
+        mensagem = mensagensDeErro[tipoDeInput][erro]
+    })
+    return mensagem
 }
 
 const validaDataNascimento = (input) => {
