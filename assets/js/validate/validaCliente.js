99ce7503126a087becbd48678140c6fb7d7f32a1
Mie <n.vanessamie@gmail.com>
2022-10-01 00:22:43 -0300

    alterações e criações de pastas

---
+++ assets/js/validate/validaCliente.js
@@ -0,0 +1,25 @@
+const dataNascimento = document.querySelector('#nascimento')
+
+dataNascimento.addEventListener('blur', (evento) => {
+    validaDataNascimento(evento.target)
+})
+
+
+const validaDataNascimento = (input) => {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior de idade para se cadastrar!'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+const maiorQue18 = (data) => {
+    const dataAtual = new Date()
+
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
